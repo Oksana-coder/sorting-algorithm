ea44b3d4667d0d0829b2fbc3b20184a505c304d0
Oksana <oksana.palamarchuk@burda-forward>
2022-05-18 13:57:11 +0200

    Adjusted for abstract class usage

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 0, 5, -5]);
-const sorter1 = new Sorter(numbersCollection);
-sorter1.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('Oksana');
-const sorter2 = new Sorter(charactersCollection);
-sorter2.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(0);
+linkedList.add(50);
+linkedList.add(-50);
+
+linkedList.print();
+
+linkedList.sort();
+linkedList.print();
